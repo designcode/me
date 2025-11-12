9dfbb22c7c17a495798f2647d3bc20ccabd25641
A Ibrahim <hello@designcode.me>
2025-11-12 11:13:03 +0100

    fix: styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,14 @@ import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://designcode.github.io",
   integrations: [mdx()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
